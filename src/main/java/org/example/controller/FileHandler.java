edb4a6da2e14b342b3e3cfaa6c16912aa673bb59
MMJimenez <munozmiguel321@gmail.com>
2022-10-25 10:55:41 +0200

    Created Filehandler

---
+++ src/main/java/org/example/controller/FileHandler.java
@@ -0,0 +1,46 @@
+package org.example.controller;
+
+import java.io.*;
+
+public class FileHandler {
+
+    private Boolean isFilePath(String filePath) {
+        var file = new File(filePath);
+        return file.exists() && !file.isDirectory();
+    }
+
+    private Boolean isDirPath(String filePath) {
+        var file = new File(filePath);
+        return file.exists() && file.isDirectory();
+    }
+
+    public String readFileTextOrVoid(String path) {
+        if (!isFilePath(path)) return "";
+
+        StringBuilder text = new StringBuilder();
+        try {
+            var bufferedReader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo");
+        }
+        return text.toString();
+    }
+
+    public void createNewFileFromText(String path, String text) {
+        var file = new File(path);
+        try {
+            if (file.createNewFile()) {
+                var bufferedWriter = new BufferedWriter(new FileWriter(file));
+                bufferedWriter.write(text);
+                bufferedWriter.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo");
+        }
+    }
+
+}
